3511ad08fab052bce61aaed000cbe6df6bcb98f6
Hairuo Guo <hairuo@mit.edu>
2016-08-20 19:50:34 -0400

    create-dataset.py debugged

--- create-dataset.py
+++ create-dataset.py
@@ -2,14 +2,15 @@
 Output json dict format:
     dict
         folder:
-            state
-                annotation:
+            state:
+                [annotation:
                     name
                     img_num (face of cube)
                     x_min
                     x_max
                     y_min
                     y_max
+                ]
             labels:
                 label:
                     end_state
@@ -24,16 +25,18 @@ def main():
     IMG_DIR = "Images/"
     ANNO_DIR = "Annotations/"
 
-    annotations = glob.glob(ANNO_DIR + "*/*.jpg")
-    poly_dict = {}    
-
+    annotations = glob.glob(ANNO_DIR + "*/*.xml")
+    data_dict = {}    
+    annos_count = 0
     for annotation in annotations:
         tree = ET.parse(annotation)
         root = tree.getroot()
         folder = root.find('folder').text
+        if folder == "practice" or folder == "example_folder":
+            continue
         if folder not in data_dict.keys():
             data_dict[folder] = {}
-        img = root.find('filename').text
+        img = root.find('filename').text.replace('.jpg', '')
         state = img.split('_')[0]
         if state not in data_dict[folder].keys():
             data_dict[folder][state] = []
@@ -41,11 +44,12 @@ def main():
         for anno_object in anno_objects:
             if int(anno_object.find('deleted').text) == 1:
                 continue
+            annos_count += 1
             anno_dict = {}
-            anno_dict['name'] = anno_name
             anno_dict['img_num'] = int(img.split('_')[1]) 
             anno_name = anno_object.find('name').text
             anno_name = anno_name.lower().replace('proposed_', '')
+            anno_dict['name'] = anno_name
             polygon = anno_object.find('polygon')
             pts = polygon.findall('pt')
             x_set = Set()
@@ -58,23 +62,31 @@ def main():
             anno_dict['min_y'] = min(y_set)
             anno_dict['max_y'] = max(y_set) 
             data_dict[folder][state].append(anno_dict)
+    labels_set = Set()
     for folder in data_dict:
-        folder['labels'] = []
-        labels_set = Set()
-        for state in folder:
-            labels = [data_dict[folder][n]['name'] for n in data_dict[folder]]
+        data_dict[folder]['labels'] = []
+        for state in data_dict[folder]:
+            if state == 'labels':
+                continue
+            labels = [n['name'] for n in data_dict[folder][state]]
             labels_set.update(labels)
     for label in labels_set:
         biggest_size = 0
         biggest_state = None
-        for state in folder:
-            size = max([(n['max_x'] - n['min_x'])*(n['max_y'] - n['min_y']) if n['name'] == label for n in state])
+        for state in data_dict[folder]:
+            if state == 'labels':
+                continue
+            bbox_areas = [(n['max_x'] - n['min_x'])*(n['max_y'] - n['min_y']) for n in data_dict[folder][state] if n['name'] == label]
+            if len(bbox_areas) != 0:
+                size = sum(bbox_areas)
+            else:
+                size = 0
             if size > biggest_size:
                 biggest_size = size
                 biggest_state = state
-        folder['labels'].append({label: biggest_state})
+        data_dict[folder]['labels'].append({label: biggest_state})
     with open('dataset_annos.json', 'w') as outfile:
-        json.dump(data_dict, outfile)  
+        json.dump([data_dict, annos_count], outfile)  
  
 
 
